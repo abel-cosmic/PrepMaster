44d2c80d0048cf4a3baf9352074b846079cfc4e6
MohammedIbrahim8887 <agentmame@gmail.com>
2023-05-04 21:14:17 +0300

    Initial commit

---
+++ PrepMaster/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import LandingPage from "./LandingPage";
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ PrepMaster/src/LandingPage.jsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+  return (
+    <div>
+      {
+        // TODO: Remove the braces and add your code here
+        // DO NOT WRITE ALL YOUR FILES HERE
+      }
+    </div>
+  );
+}
