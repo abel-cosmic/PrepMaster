3e5e0172d4f56b9c09ce2fbb6f99d1d65b6061ce
MohammedIbrahim8887 <agentmame@gmail.com>
2023-05-06 21:39:46 +0300

    Added some tutorial files

--- PrepMaster/src/LandingPage.jsx
+++ PrepMaster/src/LandingPage.jsx
@@ -1,10 +1,9 @@
+import Navigation from "./Navigation";
+
 export default function LandingPage() {
   return (
     <div>
-      {
-        // TODO: Remove the braces and add your code here
-        // DO NOT WRITE ALL YOUR FILES HERE
-      }
+      <Navigation />
     </div>
   );
 }
