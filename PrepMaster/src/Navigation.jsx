5ae5fdfc0a5f03893b318e4adf451602f87d2828
abel-cosmic <abelshibabaw@heuc.edu.et>
2023-05-08 09:10:55 +0300

    navigation bar

--- PrepMaster/src/Navigation.jsx
+++ PrepMaster/src/Navigation.jsx
@@ -1,1 +1,19 @@
-export default function Navigation() {}
+import { useEffect, useState } from "react";
+import Logo from "./assets/logo.svg";
+export default function Navigation() {
+  return (
+    <div className=" mx-10 mt-14 flex flex-row justify-between self-center  ">
+      <img src={Logo} alt="logo" className="left" />
+      <div className="flex felx-row justify-between gap-6 self-center">
+        <div>HOME</div>
+        <div>SERVICES</div>
+        <div>PRICING</div>
+        <div>CONTACT</div>
+      </div>
+      <div className="flex felx-row gap-10 justify-between items-center">
+        <div>Sign in</div>
+        <button className="button">Try for free</button>
+      </div>
+    </div>
+  );
+}
