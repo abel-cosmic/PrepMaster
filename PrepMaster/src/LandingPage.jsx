5517757093181a53932a8c24c842c6f0156d1de2
abel-cosmic <abelshibabaw@heuc.edu.et>
2023-05-08 18:48:19 +0300

    add gradient text

--- PrepMaster/src/LandingPage.jsx
+++ PrepMaster/src/LandingPage.jsx
@@ -1,9 +1,20 @@
 import Navigation from "./Navigation";
-
+import CustomButton from "./Components/CustomButton";
 export default function LandingPage() {
   return (
     <div>
       <Navigation />
+      <div className="flex flex-col items-center pt-28 gap-10">
+        <div class="gradient-text ">Start Acing Your Exams Today</div>
+        <div className="text-center description">
+          Join 10,000+ students in securing your top spot in the exit exam in
+          various fields from Computer Science to Accounting
+        </div>
+        <div>
+          <CustomButton text={"Try for free"} padding={"0.8rem 3.4rem"}/>
+        </div>
+      </div>
+
     </div>
   );
 }
--- PrepMaster/src/Navigation.jsx
+++ PrepMaster/src/Navigation.jsx
@@ -4,22 +4,35 @@ import CustomButton from "./Components/CustomButton";
 export default function Navigation() {
   return (
     <div className=" mx-12 mt-8 flex flex-row justify-between self-center  ">
-      <img src={Logo} alt="logo" className="left w-80" />
-      <ul className="flex felx-row justify-between gap-6 self-center text-xs">
-        <li>HOME</li>
-        <li>SERVICES</li>
-        <li>PRICING</li>
-        <li>CONTACT</li>
+      <img src={Logo} alt="logo" className="left w-72" />
+      <ul
+        className="flex felx-row justify-between gap-6 self-center text-xs"
+        style={{ fontWeight: "500" }}
+      >
+        <li>
+          <a href="" className="links">
+            HOME
+          </a>
+        </li>
+        <li>
+          <a href="" className="links">
+            SERVICES
+          </a>
+        </li>
+        <li>
+          <a href="" className="links">
+            PRICING
+          </a>
+        </li>
+        <li>
+          <a href="" className="links">
+            CONTACT
+          </a>
+        </li>
       </ul>
-      <div className="flex felx-row gap-10 justify-between items-center">
-        <Button
-          variant="text"
-          style={{ color: "#2e2e2e", fontWeight: "400" }}
-          sx={{ textTransform: "none" }}
-        >
-          Sign in
-        </Button>
-        <CustomButton text={"Try for free"} padding={"0.5rem 1rem"} />
+      <div className="flex felx-row gap-6 justify-between items-center">
+        <div className="sign-in">Sign in</div>
+        <CustomButton text={"Try for free"} padding={"0.5rem 1.7rem"} />
       </div>
     </div>
   );
