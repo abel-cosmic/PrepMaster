dc5d6f0efa64e04826f3b87271285fb397b201a1
abel-cosmic <abelshibabaw@heuc.edu.et>
2023-05-08 10:17:59 +0300

    adjusted the nav bar

--- PrepMaster/src/Navigation.jsx
+++ PrepMaster/src/Navigation.jsx
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
 import Logo from "./assets/logo.svg";
+import CustomButton from "./Components/CustomButton";
 export default function Navigation() {
   return (
-    <div className=" mx-10 mt-14 flex flex-row justify-between self-center  ">
-      <img src={Logo} alt="logo" className="left" />
-      <div className="flex felx-row justify-between gap-6 self-center">
-        <div>HOME</div>
-        <div>SERVICES</div>
-        <div>PRICING</div>
-        <div>CONTACT</div>
-      </div>
+    <div className=" mx-12 mt-8 flex flex-row justify-between self-center  ">
+      <img src={Logo} alt="logo" className="left w-80" />
+      <ul className="flex felx-row justify-between gap-6 self-center text-xs">
+        <li>HOME</li>
+        <li>SERVICES</li>
+        <li>PRICING</li>
+        <li>CONTACT</li>
+      </ul>
       <div className="flex felx-row gap-10 justify-between items-center">
-        <div>Sign in</div>
-        <button className="button">Try for free</button>
+        <Button
+          variant="text"
+          style={{ color: "#2e2e2e", fontWeight: "400" }}
+          sx={{ textTransform: "none" }}
+        >
+          Sign in
+        </Button>
+        <CustomButton text={"Try for free"} padding={"0.5rem 1rem"} />
       </div>
     </div>
   );
