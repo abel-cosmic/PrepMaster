081619f9500de5b1c6d44bf0b1b3395ee273d226
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-05-16 20:48:45 +0300

    Auto stash before checking out "HEAD"

---
+++ PrepMaster/src/LandingPage/FAQ.jsx
@@ -0,0 +1,10 @@
+export function FAQ(){
+    return(
+        <div>
+        <div className="text-5xl font-semibold flex flex-row justify-center content-center">Got Any
+                <span className="span"> Questions </span>{" "}? We've Got The
+                <span className="span"> The Answers </span>
+        </div>
+        </div>
+    );
+}
