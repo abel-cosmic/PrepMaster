935d20c02daf3e8fd61170f329935dc744dc7f07
Mohammed Ibrahim <agentmame@gmail.com>
2023-06-03 03:20:29 +0300

    fixes

--- PrepMaster/src/UserDashboard/Student/RenderQuestions.jsx
+++ PrepMaster/src/UserDashboard/Student/RenderQuestions.jsx
@@ -93,8 +93,8 @@ export default function RenderQuestions() {
           <p>{questions[questionNumber - 1].question}</p>
           <form onSubmit={formik.handleSubmit}>
             {choice[questionNumber - 1].map((choice) => (
-                key={choice.id}
               <div
+                key={choice.id}
                 className="flex flex-row gap-4 px-4 py-2 rounded-md my-4"
                 style={{
                   backgroundColor:
